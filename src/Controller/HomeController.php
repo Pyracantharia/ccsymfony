313d76975ac04906d40c0cb8096c56929a920e99
PERROUAS Thibault <thibaultperrouas8@gmail.com>
2024-12-21 21:33:39 +0100

    api

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,17 +6,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\ArticleRepository;
-
+use App\Service\WeatherService;
 class HomeController extends AbstractController
 {
+
+    private $weatherService;
+
+    public function __construct(WeatherService $weatherService)
+    {
+        $this->weatherService = $weatherService;
+    }
+
     #[Route('/', name: 'app_home')]
-    public function index(ArticleRepository $articleRepository): Response
+    public function index(ArticleRepository $articleRepository, WeatherService $weatherService): Response
     {
         // Récupérer tous les articles
         $articles = $articleRepository->findAll();
+        $weatherData = $weatherService->getWeatherData('Paris');
     
         return $this->render('home/index.html.twig', [
             'articles' => $articles, // Passer les articles à la vue
+            'weather' => $weatherData,
         ]);
     }
     
